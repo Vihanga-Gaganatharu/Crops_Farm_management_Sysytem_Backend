a8699f1bdecedd12d8199bde99bb30b76fbb84d7
Vihanga Gaganatharu <vihangagaganatharu1@gmail.com>
2024-12-04 11:04:28 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/crops_farm_management_sysytem_backend/CropsFarmManagementSysytemBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.crops_farm_management_sysytem_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropsFarmManagementSysytemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CropsFarmManagementSysytemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/crops_farm_management_sysytem_backend/CropsFarmManagementSysytemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.crops_farm_management_sysytem_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropsFarmManagementSysytemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
