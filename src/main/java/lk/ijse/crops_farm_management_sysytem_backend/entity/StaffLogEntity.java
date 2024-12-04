cef17ec93261028a3a3d586615aab03ba81f6efd
Vihanga Gaganatharu <vihangagaganatharu1@gmail.com>
2024-12-04 11:15:33 +0530

    Staff Section edit

---
+++ src/main/java/lk/ijse/crops_farm_management_sysytem_backend/entity/StaffLogEntity.java
@@ -0,0 +1,24 @@
+package lk.ijse.crops_farm_management_sysytem_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "staff_log")
+@Entity
+public class StaffLogEntity implements Serializable {
+    @Id
+    private String staffLogId;
+    @ManyToOne
+    @JoinColumn(name = "staff_id" ,referencedColumnName = "staffId")
+    private StaffEntity staffEntity;
+    @ManyToOne
+    @JoinColumn(name = "log_id", referencedColumnName = "logId")
+    private LogEntity logEntity;
+}
